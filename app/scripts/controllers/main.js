29727c6593ecf74d806ccc9e54e98097ab8cb06f
Wildan <garviandiwildan@gmail.com>
2016-10-28 03:15:42 +0700

    init commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hfoodidApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hfoodidApp
+ */
+angular.module('hfoodidApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
